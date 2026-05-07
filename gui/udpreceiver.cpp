324e42cc29155d06d3d7bc6539d80b8aedb534ba
x10102 <x10102.ot@gmail.com>
2026-05-07 16:13:38 +0200

    Implement UDP receiver

---
+++ gui/udpreceiver.cpp
@@ -0,0 +1,47 @@
+#include "udpreceiver.hpp"
+#include "gui/picojson.h"
+#include <QUdpSocket>
+#include <QDebug>
+#include <qglobal.h>
+#include <qhostaddress.h>
+#include <qobject.h>
+#include <qudpsocket.h>
+#include <string>
+
+UdpReceiver::UdpReceiver(QObject *parent, unsigned int port) : QObject(parent) {
+    this->port = port;
+}
+
+void UdpReceiver::start() {
+    this->sock = new QUdpSocket(this);
+    // I'm not working with IPv6 in C or C++ again, thanks for the trauma, IPK
+    this->sock->bind(QHostAddress::AnyIPv4, 6767);
+    this->isRunning = true;
+    connect(sock, &QUdpSocket::readyRead, this, &UdpReceiver::onReadyRead);
+}
+
+void UdpReceiver::terminate() {
+    this->isRunning = false;
+    if(this->sock)
+        this->sock->close();
+}
+
+void UdpReceiver::onReadyRead() {
+    while(sock->hasPendingDatagrams()) {
+        QByteArray data;
+        picojson::value json;
+        data.resize(sock->pendingDatagramSize());
+        sock->readDatagram(data.data(), data.size());
+
+        // This may not be necessary, idk if we can make picojson read it straight from the buffer
+        std::string jsonStr = QString::fromUtf8(data).toStdString();
+        std::string decodeError = picojson::parse(json, jsonStr);
+
+        if(!decodeError.empty() || !json.is<picojson::object>()) {
+            qWarning() << "Received invalid datagram, discarding it.";
+            continue;
+        }
+
+        emit dataReceived(json.get<picojson::object>());
+    }
+}
--- gui/udpreceiver.hpp
+++ gui/udpreceiver.hpp
@@ -1,8 +1,31 @@
 #ifndef _UDPREC_H
 #define _UDPREC_H
 
+#include "gui/picojson.h"
 #include <QObject>
 #include <QUdpSocket>
 
+class UdpReceiver : QObject {
+    Q_OBJECT
+public:
+    explicit UdpReceiver(QObject *parent = nullptr);
+    UdpReceiver(QObject *parent = nullptr, unsigned int port = 6767);
+
+private:
+    QUdpSocket *sock = nullptr;
+    bool isRunning = false;
+    unsigned int port = 6767;
+
+public slots:
+    void start(void);
+    void terminate(void);
+
+private slots:
+    void onReadyRead();
+
+signals:
+    void dataReceived(const picojson::object &data);
+
+};
 
 #endif
